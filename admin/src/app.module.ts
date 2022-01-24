62b23770a2c69071ebb78b1c8eedcc5191b55d2f
Shuhao Zhang <shuhao.zhang@amplexor.com>
2022-01-24 17:04:36 -0500

    establish database connection

--- admin/src/app.module.ts
+++ admin/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'password',
+          database: 'nest_microservice_admin',
+          autoLoadEntities:true,
+          synchronize: true,
+      }),
+      ProductModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
